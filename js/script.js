11f37eb2c029b569a04fe6e0c5eb759b4425e18b
YuriPereiraFonseca <yuri.pereira.fonseca25@gmail.com>
2026-03-06 20:49:24 -0300

    tornando as divs clicáveis

--- js/script.js
+++ js/script.js
@@ -5,7 +5,12 @@ for (let i = 0; i < 100; i++) {
     const elemento = document.createElement("div");
     elemento.setAttribute("class", "item");
     elemento.setAttribute("id", "item" + i);
+    elemento.setAttribute("onclick", "clique()");
     campo.appendChild(elemento);
     quadrados.push(elemento);
 
+}
+
+function clique() {
+    alert("oi")
 }
