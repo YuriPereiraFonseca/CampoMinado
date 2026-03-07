9c84bd38e65bdcb9c2c628d17806b675f11e4264
YuriPereiraFonseca <yuri.pereira.fonseca25@gmail.com>
2026-03-06 21:06:21 -0300

    criando função que sorteia bombas

--- js/script.js
+++ js/script.js
@@ -5,12 +5,32 @@ for (let i = 0; i < 100; i++) {
     const elemento = document.createElement("div");
     elemento.setAttribute("class", "item");
     elemento.setAttribute("id", "item" + i);
-    elemento.setAttribute("onclick", "clique()");
+    elemento.setAttribute("onclick", "clique(" + i + ")");
     campo.appendChild(elemento);
     quadrados.push(elemento);
 
 }
+let bombas = [];
+function sortearBombas() {
+    while (bombas.length <= 24) {
+        let bomba = Math.floor(Math.random() * (100 - 0));
+        if (!bombas.includes(bomba)) {
+            bombas.push(bomba);
+        }
+    }
+    // organizando em ordem crescente
+    bombas.sort((a, b) => a - b);
+}
 
-function clique() {
-    alert("oi")
+
+sortearBombas();
+
+console.log(bombas)
+
+function clique(i) {
+    if (bombas.includes(i)) {
+        alert("BOOM")
+    } else {
+        alert("Seguro")
+    }
 }
